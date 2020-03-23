afb8730e441442ddffba5697bc5e30deafa8d95b
yang <yangchao61@outlook.com>
2020-03-23 22:44:53 +0800

    add function for stream

--- Client.java
+++ Client.java
@@ -1,23 +1,48 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
 import java.time.LocalDate;
 import com.jasoyang.session1.*;
 import com.jasoyang.session0.*;
 import com.jasoyang.session2.*;
 
-class Client
-{
-    public static void main(String[] args)
-    {
+class Client {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         LocalDate today = LocalDate.now();
         LocalDate seasonAgo = today.minusDays(90);
-        Employee [] staff = new Employee[3];
+        Employee[] staff = new Employee[3];
         staff[0] = new Employee("jason", 500, seasonAgo);
         staff[1] = new Employee("Tom", 900, today);
-        staff[2] = new Manager("JImmy", 900, today, 2000);
+        staff[2] = new Manager("Jimmy", 900, today, 2000);
         staff[0].raiseSalary(80);
+        FileWriter fileName = new FileWriter("employee.dat", true);
+        PrintWriter out = new PrintWriter(fileName);
+        WriteData(staff, out);
+        BufferedReader bd = new BufferedReader(new FileReader("employee.dat"));
+        ReadData(bd);
+        bd.close();
+        CalendarTest.Show();
+    }
+
+    public static void WriteData(Employee[] staff, PrintWriter out)
+    {
         for(Employee e : staff)
         {
-            System.out.println(e);
+            out.println(e.toString());
         }
-        CalendarTest.Show();
+        out.flush();
+    }
+
+    public static void ReadData(BufferedReader bd) throws FileNotFoundException,IOException
+    {
+        String sentence = "";
+        while((sentence = bd.readLine()) != null)
+        {
+            System.out.println(sentence);
+        }
     }
 }
