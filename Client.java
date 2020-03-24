9be7c8bc845da0e0b60c9b344c1e67e9ec661d3a
yang <yangchao61@outlook.com>
2020-03-24 22:30:07 +0800

    use AyyayList“

--- Client.java
+++ Client.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.time.LocalDate;
+import java.util.ArrayList;
+
 import com.jasoyang.session1.*;
 import com.jasoyang.session0.*;
 import com.jasoyang.session2.*;
@@ -14,11 +16,11 @@ class Client {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         LocalDate today = LocalDate.now();
         LocalDate seasonAgo = today.minusDays(90);
-        Employee[] staff = new Employee[3];
-        staff[0] = new Employee("jason", 500, seasonAgo);
-        staff[1] = new Employee("Tom", 900, today);
-        staff[2] = new Manager("Jimmy", 900, today, 2000);
-        staff[0].raiseSalary(80);
+        ArrayList<Employee> staff = new ArrayList<>();
+        staff.add(new Employee("jason", 500, seasonAgo));
+        staff.add(new Employee("Tom", 900, today));
+        staff.add(new Manager("Jimmy", 900, today, 2000));
+        staff.get(0).raiseSalary(80);
         FileWriter fileName = new FileWriter("employee.dat", true);
         PrintWriter out = new PrintWriter(fileName);
         WriteData(staff, out);
@@ -28,11 +30,12 @@ class Client {
         CalendarTest.Show();
     }
 
-    public static void WriteData(Employee[] staff, PrintWriter out)
+    public static void WriteData(ArrayList<Employee> staff, PrintWriter out)
     {
         for(Employee e : staff)
         {
             out.println(e.toString());
+            System.out.println(e.getClass().getName());
         }
         out.flush();
     }
