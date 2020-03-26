a8bb3cfd9fe279b7285339c57d8d306d162923e4
yang <yangchao61@outlook.com>
2020-03-26 20:32:19 +0800

    complete session4 for reflect

--- com/jasoyang/session3/ReflectionTest.java
+++ com/jasoyang/session3/ReflectionTest.java
@@ -1,5 +1,8 @@
 package com.jasoyang.session3;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Constructor;
 
 public class ReflectionTest
 {
@@ -9,16 +12,60 @@ public class ReflectionTest
     }
     public void ShowCLassINfo()
     {
+        ShowContruct();
+        ShowFunction();
         ShowField();
     }
     private void ShowField()
     {
         Field[] fields = class_.getDeclaredFields();
-        System.out.println(fields.length);
-        for (Field f : fields)
+        for (Field field : fields)
         {
-            System.out.println(f.getType().getName() + " " + f.getName());
+            String modifier =Modifier.toString(field.getModifiers());
+            System.out.print(modifier + " ");
+            System.out.println(field.getType().getName() + " " + field.getName());
         }
     }
+
+    private void ShowFunction()
+    {
+        Method[] methods = class_.getMethods();
+        for(Method method : methods)
+        {
+            String modifier = Modifier.toString(method.getModifiers());
+            System.out.print(modifier + " " + method.getReturnType().getName() + " " + method.getName() + "(");
+            Class[] parameters = method.getParameterTypes();
+            for (int j = 0; j < parameters.length; j++)
+            {
+                if (j > 0)
+                {
+                    System.out.print(", ");
+                }
+                System.out.print(parameters[j].getName());
+            }
+            System.out.println(")");
+        }
+    }
+
+    private void ShowContruct()
+    {
+        Constructor[] methods = class_.getConstructors();
+        for(Constructor method : methods)
+        {
+            String modifier = Modifier.toString(method.getModifiers());
+            System.out.print(modifier + " " + method.getName() + "(");
+            Class[] parameters = method.getParameterTypes();
+            for (int j = 0; j < parameters.length; j++)
+            {
+                if (j > 0)
+                {
+                    System.out.print(", ");
+                }
+                System.out.print(parameters[j].getName());
+            }
+            System.out.println(")");
+        }
+    }
+
     private Class class_;
 }
